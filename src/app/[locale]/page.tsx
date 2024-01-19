1527888be7e87403d813261f177e3d69e60c6767
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-20 01:51:25 +0330

    updated

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,7 @@
 import TranslationsProvider from '@/components/translation/translationsProvider'
 import initTranslations from '../i18n'
 import LanguageChanger from '@/components/translation/languageChanger'
+import { Button } from '@mui/material-next'
 
 const i18nNamespaces = ['home']
 
@@ -17,7 +18,11 @@ export default async function Home({
 			resources={resources}>
 			<main className='flex min-h-screen flex-col items-center justify-between p-24'>
 				<LanguageChanger />
-				<p className='text-white text-lg'>{t('hello')}</p>
+				<button className='btn-primary'>{t('hello')}</button>
+				<div className='flex flex-col'>
+					<label className='form_label'>Input Label</label>
+					<input className='input' placeholder='input' />
+				</div>
 			</main>
 		</TranslationsProvider>
 	)
