ea7580ea87a5be2b2061befafc694f87dbabda5d
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-20 21:51:37 +0330

    remove unused components

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,5 @@
 import TranslationsProvider from '@/components/translation/translationsProvider'
 import initTranslations from '../i18n'
-import LanguageChanger from '@/components/translation/languageChanger'
-import { Button } from '@mui/material-next'
 
 const i18nNamespaces = ['home']
 
@@ -17,7 +15,6 @@ export default async function Home({
 			locale={locale}
 			resources={resources}>
 			<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-				<LanguageChanger />
 				<button className='btn-primary'>{t('hello')}</button>
 				<div className='flex flex-col'>
 					<label className='form_label'>Input Label</label>
