591772dcc362bae8847e43adc1b3883626725731
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-20 21:52:34 +0330

    add translation

--- src/app/[locale]/auth/sign-in/page.tsx
+++ src/app/[locale]/auth/sign-in/page.tsx
@@ -1,9 +1,17 @@
 import { signInAction } from '@/app/actions/auth/sign-in'
-import SubmitButton from '@/components/submitButton'
 import Image from 'next/image'
 import Link from 'next/link'
+import initTranslations from '@/app/i18n'
+import { SubmitButton } from '@/components/elements'
+const i18nNamespaces = ['auth']
 
-const SignIn = () => {
+const SignIn = async ({
+	params: { locale },
+}: {
+	params: { locale: string }
+}) => {
+	const { t } = await initTranslations(locale, i18nNamespaces)
+
 	return (
 		<div className='w-full md:h-[100vh] flex flex-col  md:flex-row'>
 			<div className='bg-[url("/images/signin.webp")] bg-fixed bg-top bg-cover md:bg-none h-[360px] md:h-full w-full md:w-[50%] overflow-hidden bg-red-50'>
@@ -22,20 +30,22 @@ const SignIn = () => {
 					className='flex-1 flex flex-col w-full sm:max-w-md justify-center gap-2'
 					action={signInAction}>
 					<div className='mb-4'>
-						<h1 className='font-bold text-3xl'>Welcome &#128075;</h1>
-						<h3 className='text-primary-200 text-sm mt-2'>Please login here</h3>
+						<h1 className='font-bold text-3xl'>{t('signin.title')} &#128075;</h1>
+						<h3 className='text-primary-200 text-sm mt-2'>
+							{t('signin.subtitle')}
+						</h3>
 					</div>
 					<label className='form_label' htmlFor='email'>
-						Email
+						{t('email')}
 					</label>
 					<input
 						className='input'
 						name='email'
-						placeholder='you@example.com'
+						placeholder={t('email_placeholder')}
 						required
 					/>
 					<label className='form_label mt-5' htmlFor='password'>
-						Password
+						{t('password')}
 					</label>
 					<input
 						className='input'
@@ -45,13 +55,13 @@ const SignIn = () => {
 						required
 					/>
 					<div className='flex mb-6'>
-						<p>dont have an account?</p>&nbsp;
+						<p>{t('signin.dont_have_account')}</p>&nbsp;
 						<Link href={'/auth/sign-up'} className='text-fuelYellow underline'>
-							Sign up
+							{t('signin.signup')}
 						</Link>
 					</div>
-					<SubmitButton className='btn-primary hover:rotate-1'>
-						Sign In
+					<SubmitButton className='btn-primary'>
+						{t('signin.submit')}
 					</SubmitButton>
 				</form>
 			</div>
--- src/app/[locale]/auth/sign-up/page.tsx
+++ src/app/[locale]/auth/sign-up/page.tsx
@@ -1,61 +1,99 @@
 import { signUpAction } from '@/app/actions/auth/sign-up'
-import SubmitButton from '@/components/submitButton'
+import initTranslations from '@/app/i18n'
+import Message from '@/components/message'
+import { SubmitButton } from '@/components/elements'
+import TranslationsProvider from '@/components/translation/translationsProvider'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const SignUp = () => {
+const i18nNamespaces = ['auth']
+
+const SignUp = async ({
+	params: { locale },
+}: {
+	params: { locale: string }
+}) => {
+	const { t, resources } = await initTranslations(locale, i18nNamespaces)
 	return (
-		<div className='w-full md:h-[100vh] flex flex-col  md:flex-row'>
-			<div className='bg-[url("/images/signup.webp")] bg-fixed bg-top bg-cover md:bg-none h-[360px] md:h-full w-full md:w-[50%] overflow-hidden bg-red-50'>
-				<Image
-					src='/images/signup.webp'
-					className='h-full w-full object-cover object-top hidden md:block'
-					alt='signup image'
-					objectFit='cover'
-					quality={100}
-					height={640}
-					width={640}
-				/>
-			</div>
-			<div className='  py-10 md:py-0 h-full w-full md:w-[50%] flex flex-col justify-center items-center px-4'>
-				<form
-					className='flex-1 flex flex-col w-full sm:max-w-md justify-center gap-2'
-					action={signUpAction}>
-					<div className='mb-4'>
-						<h1 className='font-bold text-3xl'>Create New Account</h1>
-						<h3 className='text-primary-200 text-sm mt-2'>Please enter your details</h3>
-					</div>
-					<label className='form_label mt-5' htmlFor='email'>
-						Email
-					</label>
-					<input
-						className='input'
-						name='email'
-						placeholder='you@example.com'
-						required
+		<TranslationsProvider
+			namespaces={i18nNamespaces}
+			locale={locale}
+			resources={resources}>
+			<div className='w-full md:h-[100vh] flex flex-col md:flex-row overflow-hidden'>
+				<div className='bg-[url("/images/signup.webp")] bg-fixed bg-top bg-cover md:bg-none h-[360px] md:h-full w-full md:w-[50%] overflow-hidden bg-red-50'>
+					<Image
+						src='/images/signup.webp'
+						className='h-full w-full object-cover object-top hidden md:block'
+						alt='signup image'
+						objectFit='cover'
+						quality={100}
+						height={640}
+						width={640}
 					/>
-					<label className='form_label mt-5' htmlFor='password'>
-						Password
-					</label>
-					<input
-						className='input'
-						type='password'
-						name='password'
-						placeholder='••••••••'
-						required
-					/>
-					<div className='flex mb-6'>
-						<p>already have an account?</p>&nbsp;
-						<Link href={'/auth/sign-in'} className='text-fuelYellow underline'>
-							Sign in
-						</Link>
-					</div>
-					<SubmitButton className='btn-primary hover:rotate-1'>
-						Sign Up
-					</SubmitButton>
-				</form>
+				</div>
+				<div className='  py-10 md:py-0 h-full w-full md:w-[50%] flex flex-col justify-center items-center px-4'>
+					<form
+						className='flex-1 flex flex-col w-full sm:max-w-md justify-center gap-2'
+						action={signUpAction}>
+						<div className='mb-4'>
+							<h1 className='font-bold text-3xl'>{t('signup.title')}</h1>
+							<h3 className='text-primary-200 text-sm mt-2'>
+								{t('signup.subtitle')}
+							</h3>
+						</div>
+						<label className='form_label' htmlFor='name'>
+							{t('name')}
+						</label>
+						<input
+							className='input'
+							name='name'
+							placeholder={t('name_placeholder')}
+							required
+						/>
+						<label className='form_label mt-5' htmlFor='address'>
+							{t('address')}
+						</label>
+						<input
+							className='input'
+							name='address'
+							placeholder={t('address_placeholder')}
+							required
+						/>
+						<label className='form_label mt-5' htmlFor='email'>
+							{t('email')}
+						</label>
+						<input
+							className='input'
+							name='email'
+							placeholder={t('email_placeholder')}
+							required
+						/>
+						<label className='form_label mt-5' htmlFor='password'>
+							{t('password')}
+						</label>
+						<input
+							className='input'
+							type='password'
+							name='password'
+							placeholder='••••••••'
+							required
+						/>
+						<div className='flex mb-6'>
+							<p>{t('signup.have_account')}</p>&nbsp;
+							<Link
+								href={'/auth/sign-in'}
+								className='text-fuelYellow underline'>
+								{t('signup.signin')}
+							</Link>
+						</div>
+						<SubmitButton className='btn-primary'>
+							{t('signup.submit')}
+						</SubmitButton>
+						<Message />
+					</form>
+				</div>
 			</div>
-		</div>
+		</TranslationsProvider>
 	)
 }
 
