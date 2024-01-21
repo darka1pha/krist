5272a514d8a318fcd26974288ba8cc92fd120fa6
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-22 00:30:31 +0330

    add image domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{ hostname: 'swavffutisgneenuzdoh.supabase.co', protocol: 'https' },
+		],
+	},
+}
 
 module.exports = nextConfig
