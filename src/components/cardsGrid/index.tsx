354a33332e3680013259557c25450c4f6ed7941e
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-21 20:26:50 +0330

    fix grid spacing issues

--- src/components/cardsGrid/index.tsx
+++ src/components/cardsGrid/index.tsx
@@ -1,6 +1,6 @@
 const CardsGrid = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<div className='mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 w-full'>
+		<div className='mx-auto max-w-2xl lg:max-w-7xl w-full px-8 pt-14'>
 			<div className='grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
 				{children}
 			</div>
