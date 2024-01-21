a38b597f8b376698db3d0ac52b726bba6aed5848
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-21 20:27:03 +0330

    add cards grid

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,6 +2,8 @@ import TranslationsProvider from '@/components/translation/translationsProvider'
 import initTranslations from '../i18n'
 import Hero from '@/components/hero'
 import CardsSlider from '@/components/cardsSlider'
+import CardsGrid from '@/components/cardsGrid'
+import { ProductCard } from '@/components/elements'
 
 const i18nNamespaces = ['home']
 
@@ -19,6 +21,17 @@ export default async function Home({
 			<main>
 				<Hero />
 				<CardsSlider />
+				<div className='flex justify-center items-center flex-col mt-24'>
+					<h1 className='text-5xl font-bold'>Our Best Sellers</h1>
+					<CardsGrid>
+						<ProductCard />
+						<ProductCard />
+						<ProductCard />
+						<ProductCard />
+						<ProductCard />
+						<ProductCard />
+					</CardsGrid>
+				</div>
 			</main>
 		</TranslationsProvider>
 	)
