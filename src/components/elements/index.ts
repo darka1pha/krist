ce874ba9b5f031a54e400b362b66da029a1a34e6
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-21 20:27:16 +0330

    add product card

--- src/components/elements/index.ts
+++ src/components/elements/index.ts
@@ -1,3 +1,4 @@
 export { default as SubmitButton } from './submitButton'
 export { default as IconButton } from './iconButton'
 export { default as CategoryCard } from './categoryCard'
+export { default as ProductCard } from './productCard'
