c115c3446796de0dfc5a3465bdeccc76f64402f6
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-21 19:19:56 +0330

    compelet navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -12,7 +12,7 @@ const Navbar = async ({ locale }: { locale: string }) => {
 	const { t } = await initTranslations(locale, i18nNamespaces)
 	return (
 		<nav className='flex w-full h-20 justify-between items-center px-4'>
-			<div>
+			<div className='flex-1'>
 				<Link href={'/'}>
 					<Image
 						src='/images/logo.svg'
@@ -22,12 +22,27 @@ const Navbar = async ({ locale }: { locale: string }) => {
 					/>
 				</Link>
 			</div>
-			<div className='flex justify-center items-center'>
+			<ul className='flex justify-center items-center flex-1'>
+				<li className='mx-4'>
+					<Link href={'/'}>
+						<p>{t('home')}</p>
+					</Link>
+				</li>
+				<li className='mx-4'>
+					<Link href={'/shop'}>
+						<p>{t('shop')}</p>
+					</Link>
+				</li>
+				<li className='mx-4'>
+					<Link href={'/contact-us'}>
+						<p>{t('contactus')}</p>
+					</Link>
+				</li>
+			</ul>
+
+			<div className='flex justify-end items-center flex-1'>
 				<Link href={'/favorites'}>
-					<Heart
-						className='mx-2 text-black dark:text-white'
-						size={28}
-					/>
+					<Heart className='mx-2 text-black dark:text-white' size={28} />
 				</Link>
 				<Link href={'/cart'}>
 					<ShoppingCart className='mx-2 text-black dark:text-white' size={28} />
