09dba8183f74354e6afd362fb5f49392e2bdff43
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-22 22:33:02 +0330

    add product details page

--- src/app/[locale]/product/[id]/page.tsx
+++ src/app/[locale]/product/[id]/page.tsx
@@ -1,5 +1,50 @@
-const Product = ({ params: { id } }: { params: { id: number } }) => {
-	return <div>{id}</div>
+import { Details, Gallery } from '@/components/products'
+import { Database } from '@/types/supabase'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { Metadata } from 'next'
+import { cookies } from 'next/headers'
+
+export const generateMetadata = async ({
+	params: { id },
+}: {
+	params: { id: number }
+}) => {
+	const supabase = createServerComponentClient<Database>({ cookies })
+	const { data: product } = await supabase
+		.from('products')
+		.select('*')
+		.eq('id', id)
+		.single()
+
+	const metadata: Metadata = {
+		title: product?.name,
+		description: product?.description,
+		openGraph: {
+			title: product?.name,
+			description: product?.description,
+			images: [{ url: product?.images[0] || '', alt: product?.name }],
+		},
+	}
+
+	return metadata
+}
+
+const Product = async ({ params: { id } }: { params: { id: number } }) => {
+	const supabase = createServerComponentClient<Database>({ cookies })
+	const { data: product } = await supabase
+		.from('products')
+		.select('*')
+		.eq('id', id)
+		.single()
+
+	return (
+		<main className='p-2 md:p-10'>
+			<div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+				<Gallery images={product?.images || []} />
+				<Details product={product} />
+			</div>
+		</main>
+	)
 }
 
 export default Product
