05c8f2ea6156d236d2a6e27ef8279829dea42028
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-22 17:15:42 +0330

    connect to real data

--- src/components/elements/productCard/index.tsx
+++ src/components/elements/productCard/index.tsx
@@ -1,14 +1,16 @@
 import Image from 'next/image'
 import { IconButton } from '..'
 import { Eye, Star1 } from 'iconsax-react'
+import { Tables } from '@/types/supabase'
+import Link from 'next/link'
 
-const ProductCard = () => {
+const ProductCard = ({ product }: { product: Tables<'products'> }) => {
 	return (
 		<div className='group transition-all ease-in-out'>
 			<div className='aspect-w-10 relative aspect-h-14 w-full overflow-hidden rounded-lg bg-gray-200 '>
 				<Image
 					alt='image'
-					src={'/images/signin.webp'}
+					src={product.images[0]}
 					objectFit='cover'
 					layout='fill'
 				/>
@@ -17,21 +19,21 @@ const ProductCard = () => {
 						<IconButton className='mb-2 w-fit'>
 							<Star1 size={18} className='text-primary-500' />
 						</IconButton>
-						<IconButton className='w-fit'>
-							<Eye size={18} className='text-primary-500' />
-						</IconButton>
+						<Link href={`/product/${product.id}`}>
+							<IconButton className='w-fit'>
+								<Eye size={18} className='text-primary-500' />
+							</IconButton>
+						</Link>
 					</div>
 					<div className='flex-1 w-full justify-center items-end flex p-2 md:p-4'>
-						<button className='btn-secondary w-full'>
-							Add To Cart
-						</button>
+						<button className='btn-secondary w-full'>Add To Cart</button>
 					</div>
 				</div>
 			</div>
 			<div className='pl-2 mt-1'>
-				<p className='text-lg font-bold mb-1'>Product Brand</p>
-				<p className='mb-1'>Product Name</p>
-				<p>$20.00</p>
+				<p className='text-lg font-bold mb-1'>{product.brand}</p>
+				<p className='mb-1'>{product.name}</p>
+				<p>${product.price?.toFixed(2)}</p>
 			</div>
 		</div>
 	)
