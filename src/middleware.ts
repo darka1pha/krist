828b493ac5d7a199a95a97c67913e93ab81beb85
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-24 15:11:52 +0330

    build

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,6 @@ import { withTranslation } from './middleware/translation'
 
 export default chain([withAuth, withTranslation])
 
-// only applies this middleware to files in the app directory
 export const config = {
 	matcher: '/((?!api|static|.*\\..*|_next).*)',
 }
