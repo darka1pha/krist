6a7ec6f15ee7407f04cb4dc011bcba37e33f4f8b
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-24 18:31:50 +0330

    fix page title

--- src/components/products/container/index.tsx
+++ src/components/products/container/index.tsx
@@ -5,7 +5,7 @@ import { Tables } from '@/types/supabase'
 const Container = ({ products }: { products: Tables<'products'>[] | null }) => {
 	return (
 		<div className='col-span-9'>
-			<h1 className='text-3xl font-bold '>Products</h1>
+			<h1 className='text-3xl font-bold '>Shop</h1>
 			<CardsGrid>
 				{products?.map((product, key) => (
 					<ProductCard key={key} product={product} />
