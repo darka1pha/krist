a7f992d68fce883909a068c6ca05fe664398959a
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-24 14:57:12 +0330

    add action for categories

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,62 +1,6 @@
 import { Container, Filter } from '@/components/products'
-import { Database } from '@/types/supabase'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-
-const getProducts = async ({
-	searchParams,
-}: {
-	searchParams: { [key: string]: string }
-}) => {
-	'use server'
-	const supabase = createServerComponentClient<Database>({ cookies })
-	const filterCategories = searchParams.category.split(',')
-
-	if (!!searchParams.category && !searchParams.sub) {
-		const { data: categories } = await supabase
-			.from('categories')
-			.select('id')
-			.in('name', filterCategories)
-		const categoriesId = categories?.map(({ id }) => id) ?? []
-
-		const { data: subcategories } = await supabase
-			.from('subcategories')
-			.select('id')
-			.in('category', categoriesId)
-
-		const subcategoriesId = subcategories?.map(({ id }) => id) ?? []
-
-		const data = await supabase
-			.from('products')
-			.select('*')
-			.in('category', subcategoriesId)
-		return data
-	} else if (!!searchParams.category && !!searchParams.sub) {
-		const { data: category } = await supabase
-			.from('categories')
-			.select('id')
-			.eq('name', searchParams.category)
-			.single()
-
-		const { data: subcategory } = await supabase
-			.from('subcategories')
-			.select('id')
-			.eq('category', category?.id || '')
-			.eq('name', searchParams.sub)
-			.single()
-
-		const data = await supabase
-			.from('products')
-			.select('*')
-			.eq('category', subcategory?.id || '')
-
-		return data
-	} else {
-		const data = await supabase.from('products').select('*')
-
-		return data
-	}
-}
+import { CategoriesProps } from '@/types'
+import { getCategories, getProducts } from '../actions'
 
 const Products = async ({
 	searchParams,
@@ -65,9 +9,11 @@ const Products = async ({
 }) => {
 	const { data: products } = await getProducts({ searchParams })
 
+	const categories = (await getCategories()) as CategoriesProps[]
+
 	return (
 		<main className='grid grid-cols-12 gap-5 min-h-screen paddings'>
-			<Filter />
+			<Filter categories={categories} />
 			<Container products={products} />
 		</main>
 	)
--- src/components/products/filter/index.tsx
+++ src/components/products/filter/index.tsx
@@ -1,14 +1,7 @@
 import { CategoriesProps } from '@/types'
 import ProductsCategory from './productsCategory'
 
-const getCategories = async () => {
-	const categories = await fetch('https://krist.vercel.app/api/categories')
-	return await categories.json()
-}
-
-const Filter = async () => {
-	const categories = (await getCategories()) as CategoriesProps[]
-console.log(categories)
+const Filter = ({ categories }: { categories: CategoriesProps[] }) => {
 	return (
 		<div className='col-span-3'>
 			<ProductsCategory categories={categories} />
