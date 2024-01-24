016007c3f28404811c1acd71fc3fe30c6a9077ac
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-24 15:10:11 +0330

    fix bugs

--- src/components/products/filter/index.tsx
+++ src/components/products/filter/index.tsx
@@ -1,15 +1,7 @@
 import { CategoriesProps } from '@/types'
 import ProductsCategory from './productsCategory'
 
-const getCategories = async () => {
-	const categories = await fetch('https://krist.vercel.app/api/categories')
-
-	return await categories.json()
-}
-
-const Filter = async () => {
-	const categories = (await getCategories()) as CategoriesProps[]
-
+const Filter = async ({ categories }: { categories: CategoriesProps[] }) => {
 	return (
 		<div className='col-span-3'>
 			<ProductsCategory categories={categories} />
