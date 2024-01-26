1724f66b016c040006022847ceaa32f0edff89dd
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-26 18:51:09 +0330

    fix type error

--- src/components/profile/personalInfo/avatar/index.tsx
+++ src/components/profile/personalInfo/avatar/index.tsx
@@ -9,7 +9,7 @@ const Avatar = ({
 }) => {
 	return (
 		<div className='mb-10 w-fit relative'>
-			<Profile className='h-20 w-20' src={avatarUrl} alt={name} />
+			<Profile className='h-20 w-20' src={avatarUrl || ''} alt={name} />
 			<button className='h-8 w-8 rounded-xl flex justify-center items-center bg-primary-500 absolute -bottom-2 -right-2'>
 				<Edit2 size={15} className='text-white' />
 			</button>
