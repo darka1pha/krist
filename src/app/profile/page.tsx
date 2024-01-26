19c948d86f8b4b92df6c79735c54eaa4f9c4cab3
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-26 19:56:38 +0330

    add edit modal

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,17 @@
 import { PersonalInfo } from '@/components/profile'
+import EditModal from '@/components/profile/personalInfo/editModal'
 
 const Profile = async ({
 	params: { locale },
+	searchParams: { edit },
 }: {
 	params: { locale: string }
+	searchParams: { edit: string }
 }) => {
 	return (
 		<div className='col-span-9'>
 			<PersonalInfo locale={locale} />
+			{edit === 'true' && <EditModal locale={locale} />}
 		</div>
 	)
 }
