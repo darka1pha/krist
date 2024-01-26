1a4f7bf16b1d4cc90b690c85ce4729d3b013f8cc
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-26 18:44:45 +0330

    fix profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,23 +1,13 @@
-import { Menu, PersonalInfo } from '@/components/profile'
-import { Database } from '@/types/supabase'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
+import { PersonalInfo } from '@/components/profile'
 
 const Profile = async ({
 	params: { locale },
 }: {
 	params: { locale: string }
 }) => {
-	const supabase = createServerComponentClient<Database>({ cookies })
-	const {
-		data: { user },
-	} = await supabase.auth.getUser()
-
-	const { data } = await supabase.from('profiles').select('*').single()
-
 	return (
-		<div>
-			<PersonalInfo />
+		<div className='col-span-9'>
+			<PersonalInfo locale={locale} />
 		</div>
 	)
 }
