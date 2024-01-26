5e4a9cefbc210828bba2d0ac98859ce6ae5f6e38
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-26 18:45:14 +0330

    add personal info component

--- src/components/profile/personalInfo/index.tsx
+++ src/components/profile/personalInfo/index.tsx
@@ -1,5 +1,26 @@
-const PersonalInfo = () => {
-	return <div className='col-span-9 bg-red-300'>Enter</div>
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import Avatar from './avatar'
+import InfoForm from './infoForm'
+import { Database } from '@/types/supabase'
+import { cookies } from 'next/headers'
+
+const PersonalInfo = async ({ locale }: { locale: string }) => {
+	const supabase = createServerComponentClient<Database>({ cookies })
+	const { data } = await supabase.from('profiles').select('*').single()
+	const {
+		data: { user },
+	} = await supabase.auth.getUser()
+	return (
+		<div className='col-span-9'>
+			<Avatar name={data?.name} avatarUrl={data?.avatar_url} />
+			<InfoForm
+				name={data?.name}
+				address={data?.address}
+				email={user?.email}
+				locale={locale}
+			/>
+		</div>
+	)
 }
 
 export default PersonalInfo
