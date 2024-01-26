f6242c1330ffd44bf351c50668c1a6ef4e69c90c
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-26 19:56:51 +0330

    fix avatar styles

--- src/components/profile/personalInfo/avatar/index.tsx
+++ src/components/profile/personalInfo/avatar/index.tsx
@@ -1,5 +1,6 @@
 import { Avatar as Profile } from '@mui/material'
 import { Edit2 } from 'iconsax-react'
+import Link from 'next/link'
 const Avatar = ({
 	name,
 	avatarUrl,
@@ -8,11 +9,21 @@ const Avatar = ({
 	avatarUrl: string | undefined | null
 }) => {
 	return (
-		<div className='mb-10 w-fit relative'>
-			<Profile className='h-20 w-20' src={avatarUrl || ''} alt={name} />
-			<button className='h-8 w-8 rounded-xl flex justify-center items-center bg-primary-500 absolute -bottom-2 -right-2'>
-				<Edit2 size={15} className='text-white' />
-			</button>
+		<div className='mb-10 flex justify-between items-center'>
+			<div className='relative w-fit'>
+				<Profile className='h-20 w-20' src={avatarUrl || ''} alt={name} />
+				<button className='h-8 w-8 rounded-xl flex justify-center items-center bg-primary-500 absolute -bottom-2 -right-2'>
+					<Edit2 size={15} className='text-white' />
+				</button>
+			</div>
+			<div>
+				<Link href={'/profile?edit=true'}>
+					<button className='flex btn-primary items-center'>
+						<Edit2 size={18} className='text-white mr-2' />
+						Edit Profile
+					</button>
+				</Link>
+			</div>
 		</div>
 	)
 }
