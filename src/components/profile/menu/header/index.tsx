38f68e46217150913903486e3fd6138b680ee799
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-26 13:02:46 +0330

    fix type error

--- src/components/profile/menu/header/index.tsx
+++ src/components/profile/menu/header/index.tsx
@@ -1,6 +1,6 @@
 import { Avatar } from '@mui/material'
 
-const Header = ({ name }: { name: string | null }) => {
+const Header = ({ name }: { name: string | undefined }) => {
 	return (
 		<div className='p-4 border-2 border-gray-300 w-full mt-10 flex items-center rounded-xl'>
 			<div>
