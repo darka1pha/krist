4b88784ee40d10de963e3da04c1de8ff27ff4ccf
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-30 00:50:28 +0330

    add favorites page

--- src/app/profile/favorites/page.tsx
+++ src/app/profile/favorites/page.tsx
@@ -1,9 +1,28 @@
-const Favorites = () => {
-  return (
-    <div>
-      Enter
-    </div>
-  );
+import CardsGrid from '@/components/cardsGrid'
+import ProductCard from '@/components/elements/productCard'
+import { Database } from '@/types/supabase'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+const Favorites = async () => {
+	const supabase = createServerComponentClient<Database>({ cookies })
+	const { data: favorites } = await supabase.from('favorites').select(
+		`
+  *,
+  products (*)
+  `
+	)
+
+	return (
+		<div className='col-span-9'>
+			<h1 className='text-3xl font-bold '>Favorites</h1>
+			<CardsGrid>
+				{favorites?.map(({ products: product }, key) => (
+					<ProductCard key={key} product={product!} />
+				))}
+			</CardsGrid>
+		</div>
+	)
 }
 
-export default Favorites;
+export default Favorites
