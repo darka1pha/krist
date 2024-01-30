9c3fc0771b78a02aa08d0191f2802b65e2d2696d
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-30 21:52:27 +0330

    add cart icon to navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,11 +2,19 @@ import Link from 'next/link'
 import Image from 'next/image'
 import initTranslations from '@/app/i18n'
 import AuthButton from './authButton'
+import CartIcon from '../elements/cartIcon'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { Database } from '@/types/supabase'
 
 const i18nNamespaces = ['navbar']
 
 const Navbar = async ({ locale }: { locale: string }) => {
 	const { t } = await initTranslations(locale, i18nNamespaces)
+	const supabase = createServerComponentClient<Database>({ cookies })
+	const {
+		data: { user },
+	} = await supabase.auth.getUser()
 	return (
 		<nav className='flex w-full h-20 justify-between items-center px-4'>
 			<div className='flex-1'>
@@ -36,7 +44,10 @@ const Navbar = async ({ locale }: { locale: string }) => {
 					</Link>
 				</li>
 			</ul>
-			<AuthButton locale={locale} />
+			<div className='flex items-center flex-1 justify-end'>
+				{user && <CartIcon />}
+				<AuthButton user={user} locale={locale} />
+			</div>
 		</nav>
 	)
 }
