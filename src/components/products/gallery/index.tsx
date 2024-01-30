03eaab43c26b3c1a8c5bf95b6077ab6fc2d78b3a
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-31 00:09:49 +0330

    fix swiper error

--- src/components/products/gallery/index.tsx
+++ src/components/products/gallery/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Swiper, SwiperSlide } from 'swiper/react'
+import { Swiper, SwiperSlide, SwiperRef } from 'swiper/react'
 
 // Import Swiper styles
 import 'swiper/css'
@@ -9,19 +9,22 @@ import 'swiper/css/navigation'
 import 'swiper/css/thumbs'
 
 import { FreeMode, Navigation, Thumbs } from 'swiper/modules'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { Swiper as SwiperType } from 'swiper'
 import Thumbnail from './thumbnail'
 import GallerySlide from './gallerySlide'
 
 const Gallery = ({ images }: { images: string[] }) => {
-	const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>(null)
+	const [swiper, setSwiper] = useState<SwiperType | null>(null)
+	const slideTo = (index: number) => {
+		swiper?.slideTo(index)
+	}
 	return (
 		<div className='px-8 md:px-0'>
 			<Swiper
+				onSwiper={setSwiper}
 				spaceBetween={10}
 				navigation={true}
-				thumbs={{ swiper: thumbsSwiper }}
 				modules={[FreeMode, Navigation, Thumbs]}>
 				{images.map((image, key) => (
 					<SwiperSlide key={key}>
@@ -30,12 +33,15 @@ const Gallery = ({ images }: { images: string[] }) => {
 				))}
 			</Swiper>
 			<Swiper
-				onSwiper={(swiper) => {
-					setThumbsSwiper(swiper)
-				}}
 				slidesPerView={'auto'}
 				spaceBetween={30}
 				freeMode={true}
+				centeredSlides={true}
+				slideToClickedSlide={true}
+				onActiveIndexChange={(swiper) => {
+					slideTo(swiper.activeIndex)
+				}}
+				initialSlide={0}
 				watchSlidesProgress={true}
 				modules={[FreeMode, Navigation, Thumbs]}
 				className='mt-5 h-20'>
