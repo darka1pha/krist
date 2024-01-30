939fa1d0f4355d65ff7f0a2eb16fc4b7b2d9dc42
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-30 13:38:44 +0330

    add pointer to slider thumbnails

--- src/components/products/gallery/thumbnail.tsx
+++ src/components/products/gallery/thumbnail.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const Thumbnail = ({ url }: { url: string }) => {
 	return (
-		<div className='h-20 w-20 overflow-hidden rounded-xl'>
+		<div className='h-20 w-20 overflow-hidden rounded-xl cursor-pointer'>
 			<Image
 				alt='thumbnail'
 				className='w-full h-full'
