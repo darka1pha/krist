6af8b216fb164428efbb1d3489f363e63a7fc3b5
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-01-30 21:53:42 +0330

    updated

--- src/components/navbar/authButton/index.tsx
+++ src/components/navbar/authButton/index.tsx
@@ -1,20 +1,26 @@
 import initTranslations from '@/app/i18n'
 import { Database } from '@/types/supabase'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import {
+	User,
+	createServerComponentClient,
+} from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import Link from 'next/link'
 import AccountMenu from '../accountMenu'
 
 const i18nNamespaces = ['navbar']
 
-const AuthButton = async ({ locale }: { locale: string }) => {
+const AuthButton = async ({
+	locale,
+	user,
+}: {
+	locale: string
+	user: User | null
+}) => {
 	const { t } = await initTranslations(locale, i18nNamespaces)
-	const supabase = createServerComponentClient<Database>({ cookies })
-	const {
-		data: { user },
-	} = await supabase.auth.getUser()
+
 	return (
-		<div className='flex justify-end items-center flex-1'>
+		<div className='flex justify-end items-center'>
 			{!user ? (
 				<Link href={'/auth/sign-in'}>
 					<button className='btn-primary ml-2 w-24'>{t('signin')}</button>
