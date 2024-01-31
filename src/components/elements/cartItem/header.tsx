eaf94b8e1648015e7baa87a70b66c6c5d032a30c
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-02-01 01:21:31 +0330

    fix spacing

--- src/components/elements/cartItem/header.tsx
+++ src/components/elements/cartItem/header.tsx
@@ -1,20 +1,20 @@
 const Header = () => {
 	return (
 		<div className='p-4 border-b border-gray-[#f6f6f6] grid grid-cols-12 grid-flow-row'>
-			<span className='col-span-1'>
+			<span className='col-span-8 md:col-span-1'>
 				<h2 className='text-lg font-bold mb-2'>Products</h2>
 			</span>
-			<span className='px-2 col-span-5'></span>
-			<span className='col-span-1'>
+			<span className='px-2 col-span-4'></span>
+			<span className='grid col-span-3 md:col-span-1 place-items-center'>
 				<h3 className='grid text-md justify-items-center'>Price</h3>
 			</span>
-			<span className='grid col-span-3 justify-items-center'>
+			<span className='grid col-span-4 md:col-span-3 place-items-center'>
 				<h3 className='text-md'>Quantity</h3>
 			</span>
-			<span className='grid col-span-1 justify-items-center'>
+			<span className='grid col-span-3  md:col-span-1 place-items-center'>
 				<h3 className='text-md'>Subtotal</h3>
 			</span>
-			<span className='grid col-span-1'></span>
+			<span className='col-span-2'></span>
 		</div>
 	)
 }
