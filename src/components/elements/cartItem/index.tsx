0a376fafabc0cfb2b8f8d4ecb3887145e5026fc4
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-02-01 01:21:42 +0330

    fix spacing

--- src/components/elements/cartItem/index.tsx
+++ src/components/elements/cartItem/index.tsx
@@ -14,8 +14,8 @@ type CartItemProps = {
 }
 const CartItem = ({ brand, id, name, price, qty, image }: CartItemProps) => {
 	return (
-		<div className='p-4 border-b border-gray-[#f6f6f6] grid grid-cols-12 grid-flow-row'>
-			<span className='h-20 overflow-hidden relative rounded-lg col-span-1'>
+		<div className='p-4 border-b border-gray-[#f6f6f6] grid grid-cols-12 grid-flow-row gap-4'>
+			<span className='h-20 overflow-hidden relative rounded-lg col-span-3 sm:col-span-2  md:col-span-1'>
 				<Image
 					src={image}
 					alt='product image'
@@ -23,20 +23,20 @@ const CartItem = ({ brand, id, name, price, qty, image }: CartItemProps) => {
 					className='object-cover h-full w-full'
 				/>
 			</span>
-			<span className='px-2 col-span-5 ml-2'>
+			<span className='px-2 col-span-9 sm:col-span-10 md:col-span-4 ml-2'>
 				<h2 className='text-lg font-bold mb-2'>{name}</h2>
 				<h2 className='text-md'>{brand}</h2>
 			</span>
-			<span className='grid col-span-1 place-items-center'>
+			<span className='grid col-span-3 md:col-span-1 place-items-center'>
 				<h3 className='text-md'>${price.toFixed(2)}</h3>
 			</span>
-			<span className='grid col-span-3 place-items-center'>
+			<span className='grid col-span-4 md:col-span-3 place-items-center'>
 				<Quantity id={id} qty={qty} />
 			</span>
-			<span className='grid col-span-1 place-items-center'>
+			<span className='grid col-span-3  md:col-span-1 place-items-center'>
 				<h3 className='text-md'>${(price * qty).toFixed(2)}</h3>
 			</span>
-			<span className='grid col-span-1 items-center justify-items-end'>
+			<span className='grid col-span-2 items-center justify-items-end'>
 				<form action={deleteCartItemAction}>
 					<input type='hidden' name='id' value={id} />
 					<IconButton>
