02bf64996017c0fab37a9333ba745d2eda1e67de
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-02-01 01:21:22 +0330

    add shopping cart details

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,3 +1,4 @@
+import { SubmitButton } from '@/components/elements'
 import CartItem from '@/components/elements/cartItem'
 import Header from '@/components/elements/cartItem/header'
 import { Database } from '@/types/supabase'
@@ -6,6 +7,10 @@ import { cookies } from 'next/headers'
 
 const CartPage = async () => {
 	const supabase = createServerComponentClient<Database>({ cookies })
+	const { data: cart } = await supabase
+		.from('shopping_cart')
+		.select('*')
+		.single()
 	const { data } = await supabase
 		.from('cart_items')
 		.select(
@@ -16,11 +21,13 @@ const CartPage = async () => {
 		)
 		.order('created_at')
 
+	console.log(cart)
+
 	return (
 		<main className='paddings'>
 			<h1 className='text-3xl font-bold'>Cart Details</h1>
-			<span className='grid grid-cols-12'>
-				<div className='col-span-8 mt-4'>
+			<span className='grid grid-cols-12 gap-5'>
+				<div className='col-span-12 lg:col-span-8 mt-4'>
 					<Header />
 					{data?.map(({ id, qty, products }) => (
 						<CartItem
@@ -33,7 +40,41 @@ const CartPage = async () => {
 						/>
 					))}
 				</div>
-				<div className='col-span-4'></div>
+				<span className='col-span-12 lg:col-span-4 p-4 border border-[#f6f6f6] rounded-xl shadow-lg h-fit'>
+					<div className='border-b border-gray-200 flex items-center justify-between py-4'>
+						<h3 className='text-xl font-bold'>Subtotal</h3>
+						<h3 className='text-xl font-bold'>
+							${cart?.total_price?.toFixed(2)}
+						</h3>
+					</div>
+					<div className='border-b border-gray-200 flex items-center justify-between py-4'>
+						<h3 className='text-lg'>Delivery Charge</h3>
+						<h3 className='text-lg'>${(20).toFixed(2)}</h3>
+					</div>
+					<div className='flex items-center justify-between py-4'>
+						<h3 className='text-xl font-bold'>Grand Total</h3>
+						<h3 className='text-xl font-bold'>
+							$
+							{cart && cart.total_price
+								? (cart.total_price + 20).toFixed(2)
+								: 0}
+						</h3>
+					</div>
+					<form className='w-full mt-5'>
+						<input
+							name='total_price'
+							type='hidden'
+							value={
+								cart && cart.total_price
+									? (cart.total_price + 20).toFixed(2)
+									: 0
+							}
+						/>
+						<SubmitButton className='btn-primary w-full'>
+							Process To Checkout
+						</SubmitButton>
+					</form>
+				</span>
 			</span>
 		</main>
 	)
