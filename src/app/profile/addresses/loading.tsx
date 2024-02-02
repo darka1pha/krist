c2c79f16fe6becc843297730988ff574974ce481
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-02-02 22:19:28 +0330

    addresses loading

---
+++ src/app/profile/addresses/loading.tsx
@@ -0,0 +1,23 @@
+const AddressesLoading = () => {
+	return (
+		<div className='col-span-9 w-full'>
+			<div className='flex w-full justify-between items-center'>
+				<div
+					about='name'
+					className='h-16 w-40 bg-gray-200 animate-pulse rounded-xl'
+				/>
+				<div
+					about='new address'
+					className='h-16 w-16 bg-gray-200 animate-pulse rounded-xl'
+				/>
+			</div>
+			<div className='mt-2 w-full'>
+				{Array.from(new Array(4)).map((key) => (
+					<div key={key} className='my-4 bg-gray-200 animate-pulse rounded-xl w-full h-36' />
+				))}
+			</div>
+		</div>
+	)
+}
+
+export default AddressesLoading
