fafe39a882dee6f3729dae72a4de9edfda0dff3c
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-02-02 13:53:40 +0330

    testing og image

--- src/app/products/[id]/opengraph-image.tsx
+++ src/app/products/[id]/opengraph-image.tsx
@@ -1,65 +1,34 @@
-import { Database } from '@/types/supabase'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import Image from 'next/image'
 import { ImageResponse } from 'next/og'
-
-// Route segment config
+ 
 export const runtime = 'edge'
-
-// Image metadata
+ 
 export const alt = 'About Acme'
 export const size = {
-	width: 920,
-	height: 1280,
+  width: 1200,
+  height: 630,
 }
-
 export const contentType = 'image/png'
-
-// Image generation
-export default async function og({
-	params: { id },
-}: {
-	params: { id: number }
-}) {
-	const supabase = createServerComponentClient<Database>({ cookies })
-	const { data: product } = await supabase
-		.from('products')
-		.select('*')
-		.eq('id', id)
-		.single()
-
-	return new ImageResponse(
-		(
-			<div
-				style={{
-					fontSize: 32,
-					background: 'white',
-					width: '920px',
-					height: '1280px',
-					display: 'flex',
-					alignItems: 'center',
-					justifyContent: 'center',
-					borderRadius: '20px',
-					backgroundColor: 'beige',
-					backgroundImage: `url(${product?.images[0]!})`,
-					backgroundPosition: 'top',
-					backgroundSize: '100% 100%',
-					backgroundRepeat: 'no-repeat',
-				}}>
-				<p
-					style={{
-						backgroundColor: '#ffffff70',
-						padding: 20,
-						borderRadius: 20,
-						fontWeight: 'bold',
-					}}>
-					{product?.name}
-				</p>
-			</div>
-		),
-		{
-			...size,
-		}
-	)
+ 
+export default async function Image({ params }: { params: { id: string } }) {
+ 
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 48,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        This is Just a Test
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
 }
