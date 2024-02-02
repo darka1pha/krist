3ce4b356c165d9c6452ccb369bf2dc08a9ff4ee3
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-02-02 12:27:48 +0330

    add redirects to protected route

--- src/app/cart/layout.tsx
+++ src/app/cart/layout.tsx
@@ -1,32 +1,16 @@
-import { Menu } from '@/components/profile'
 import { Database } from '@/types/supabase'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
-const ProfileLayout = async ({
-	params: { locale },
-	children,
-}: {
-	children: React.ReactNode
-	params: { locale: string }
-}) => {
+const CartLayout = async ({ children }: { children: React.ReactNode }) => {
 	const supabase = createServerComponentClient<Database>({ cookies })
 	const {
 		data: { user },
 	} = await supabase.auth.getUser()
 
 	if (!user) redirect('/auth/sign-in')
-
-	return (
-		<main className='paddings'>
-			<div className='grid gap-5 grid-cols-12 w-full'>
-				<Menu locale={locale} />
-				{children}
-			</div>
-		</main>
-	)
+	return { children }
 }
 
-export default ProfileLayout
+export default CartLayout
